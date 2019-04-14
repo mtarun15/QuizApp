95a58747592bdea71881ab230033b78d370582ef
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-04-14 12:25:58 +0530

    adding functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question';
+import quizQuestions from './api/quizQuestions';
 
+constructor(props); {
+  super(props);
 
+  this.state = {
+    counter: 0,
+    questionId: 1,
+    question: '',
+    answerOptions: [],
+    answer: '',
+    answersCount: {
+      nintendo: 0,
+      microsoft: 0,
+      sony: 0
+    },
+    result: ''
+  };
+}
 class App extends Component {
   render() {
     return (
